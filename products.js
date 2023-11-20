f7eb83a979a2ac31e0855f577ad74333b1e8ebe4
uqba <uqbashaikh999@gmail.com>
2023-11-20 17:07:06 +0400

    used different products

---
+++ products.js
@@ -0,0 +1,106 @@
+let products = [
+    {
+        "id": 1001,
+        "subject": "soccer",
+        "location": "dubai internet city sports club",
+        "price": 5000,
+        "image": "images/soccer.jpg",
+        "space": "5",
+        "availableInventory": 5,
+        "rating": 3
+
+
+    },
+    {
+        "id": 1002,
+        "subject": "boxing",
+        "location": "abu dhabi arena",
+        "price": 9000,
+        "image": "images/boxing.jpg",
+        "space": "4",
+        "availableInventory": 4,
+        "rating": 4
+
+    },
+    {
+        "id": 1003,
+        "subject": "photography",
+        "location": "national park",
+        "price": 7000,
+        "image": "images/photography.jpg",
+        "space": "4",
+        "availableInventory": 4,
+        "rating": 4
+    },
+    {
+        "id": 1004,
+        "subject": "camping",
+        "location": "jabal jais",
+        "price": 2000,
+        "image": "images/camping.jpg",
+        "space": "4",
+        "availableInventory": 4,
+        "rating": 4
+    },
+    {
+        "id": 1005,
+        "subject": "dance",
+        "location": "wollongong university",
+        "price": 1000,
+        "image": "images/dancing.jpg",
+        "space": "4",
+        "availableInventory": 4,
+        "rating": 4
+    },
+    {
+        "id": 1006,
+        "subject": "robotics",
+        "location": "sharjah fair",
+        "price": 2000,
+        "image": "images/robotics.jpg",
+        "space": "4",
+        "availableInventory": 4,
+        "rating": 4
+    },
+    {
+        "id": 1007,
+        "subject": "coding clubs",
+        "location": "middlesex university",
+        "price": 1000,
+        "image": "images/coding.jpg",
+        "space": "4",
+        "availableInventory": 4,
+        "rating": 4
+    },
+    {
+        "id": 1008,
+        "subject": "fashion club",
+        "location": "expo city",
+        "price": 3000,
+        "image": "images/fashion.jpg",
+        "space": "4",
+        "availableInventory": 4,
+        "rating": 4
+    },
+    {
+        "id": 1009,
+        "subject": "cooking",
+        "location": "culinary school",
+        "price": 4000,
+        "image": "images/cooking.jpg",
+        "space": "4",
+        "availableInventory": 4,
+        "rating": 4
+    },
+    {
+        "id": 1010,
+        "subject": "film making",
+        "location": "dubai design district",
+        "price": 9000,
+        "image": "images/film.jpg",
+        "space": "4",
+        "availableInventory": 4,
+        "rating": 4
+    },
+
+]
